8e154fb2a2907e239c6ecbbcb64796abbed4b6e5
Shorya Pawar <22shorya23@gmail.com>
2023-01-05 23:47:21 +0530

    DAY 05 uploaded

---
+++ JAN 05/q.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+int convertFive(int n);
+
+// Driver program to test above function
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n;
+        cout << convertFive(n) << endl;
+    }
+}
+// } Driver Code Ends
+
+
+/*you are required to complete this method*/
+int convertFive(int n) {
+    // Your code here
+    string s = to_string(n) + "";
+    replace(s.begin(),s.end(),'0','5');
+    return stoi(s);
+    
+    
+
+    
+}
